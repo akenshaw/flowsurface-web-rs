db4ceb1870316f43666ea7a956259a08285b242e
Berke <63060680+akenshaw@users.noreply.github.com>
2024-03-28 18:21:47 +0300

    renamed:    www/dist/b24083cd5f1844fde50f.module.wasm -> www/dist/b360095b1d00b6010a4b.module.wasm 	modified:   www/dist/bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -315,7 +315,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/rsdepth_bg.wasm":"b24083cd5f1844fde50f"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/rsdepth_bg.wasm":"b360095b1d00b6010a4b"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
